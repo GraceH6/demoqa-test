96137afbd79409e868db9430facd01825cdf0353
Zhansaya Kalsat <zhansayakalsat@gmail.com>
2023-05-31 14:50:27 +0600

    CalendarComponent changed

--- src/test/java/com/demoqa/pages/components/CalendarComponent.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponent.java
@@ -6,7 +6,7 @@ public class CalendarComponent {
     public void setDate(String year, String month, String day) {
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__month-select").selectOption(month);
-        String dayPickerLocator = String.format(".react-datepicker__day--0" + day);
+        String dayPickerLocator = String.format(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)");
         $(dayPickerLocator).click();
     }
 }
